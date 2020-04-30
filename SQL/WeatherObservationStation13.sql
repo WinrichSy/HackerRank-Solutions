225fe01eb5b63a0c73a2ba6738b90d791a212013
Winrich Sy <winrichsy@gmail.com>
2020-04-30 16:37:56 -0700

    added more SQL Hackerrank solutions

---
+++ SQL/WeatherObservationStation13.sql
@@ -0,0 +1,6 @@
+--Weather Observation Station 13
+--https://www.hackerrank.com/challenges/weather-observation-station-13/problem
+
+SELECT ROUND(SUM(LAT_N), 4)
+FROM station
+WHERE LAT_N>38.7880 and LAT_N<137.2345;
---
+++ SQL/WeatherObservationStation15.sql
@@ -0,0 +1,8 @@
+--Weather Observation Station 15
+--https://www.hackerrank.com/challenges/weather-observation-station-15/problem
+
+SELECT ROUND(LONG_W, 4)
+FROM station
+WHERE LAT_N = (SELECT MAX(LAT_N)
+                     FROM station
+                     WHERE LAT_N<137.2345);
