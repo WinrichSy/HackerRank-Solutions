36383db66ac3999f49fb05c22bb286d0b798af31
Winrich Sy <winrichsy@gmail.com>
2020-04-30 16:36:04 -0700

    added SQL hackerrank solutions

---
+++ SQL/PopulationDensityDifference.sql
@@ -0,0 +1,5 @@
+--Population Density Difference
+--https://www.hackerrank.com/challenges/population-density-difference/problem
+
+SELECT MAX(population)-MIN(population)
+FROM city;
---
+++ SQL/RevisingAggregations-Averages.sql
@@ -0,0 +1,6 @@
+--Revising Aggregations - Averages
+--https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE district = 'California'
